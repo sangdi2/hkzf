11805543a16e5a5f82636eb7a022ebae99e15828
sangdi <1592807515@qq.com>
2022-05-07 15:26:36 +0800

    rth

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'antd-mobile'
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
+function App() {
+  return (
+    <div>
+      <Button>登录</Button>
+    </div>
+  );
+}
+
+export default App;
