4ed3cb639696601553675f83f56103f19de1d18f
sangdi <1592807515@qq.com>
2022-05-15 11:40:08 +0800

    rth

--- src/pages/Citylist/index.js
+++ src/pages/Citylist/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavBar, Icon } from 'antd-mobile';
+import { NavBar, Icon,Toast } from 'antd-mobile';
 import './index.scss'
 import axios from "axios";
 import {getcurrentcity} from '../../utils'
@@ -16,6 +16,10 @@ import {List,AutoSizer} from 'react-virtualized';
                return letter.toUpperCase()
        }
   }
+  const title=36
+  const rowheight=52
+
+  const ci=["北京","上海","广州","深圳"]
   
 
 const chulilist=list=>{
@@ -36,10 +40,24 @@ const chulilist=list=>{
    }
 }
 export default class Citylist extends React.Component{
-    state={
-        citylist:{},
-        cityindex:[]
+    constructor(props){
+        super(props)
+        this.state={
+            citylist:{},
+            cityindex:[],
+            activeIndex:0
+        }
+        this.cityListComponent=React.createRef()
     }
+    changcity=({label,value})=>{
+      if(ci.indexOf(label)>-1){
+        localStorage.setItem('hkzf_localcity',JSON.stringify({label,value}))
+        this.props.history.go(-1)
+      }else{
+        Toast.info("该房源暂无数据",1)
+      }
+    }
+    
      rowRenderer=({
         key, // Unique key within array of rows
         index, // Index of row within collection
@@ -47,17 +65,41 @@ export default class Citylist extends React.Component{
         isVisible, // This row is visible within the List (eg it is not an overscanned row)
         style, // Style object to be applied to row (to position it)
       }) =>{
-        const { cityindex} = this.state
-        const letter = cityindex[index]
+        // const { cityindex} = this.state
+        const letter = this.state.cityindex[index]
+        
         return (
           <div key={key} style={style} className="city">
             <div className="title">{formatletter(letter)}</div>
-            <div className="name">上海</div>
+            {this.state.citylist[letter].map(item=><div className="name" key={item.value} onClick={()=>this.changcity(item)}>{item.label}</div>)}
+            
           </div>
         );
       }
-    componentDidMount(){
-        this.getcity()
+      getrowheight=({index})=>{
+          return title+rowheight*this.state.citylist[this.state.cityindex[index]].length
+      }
+      renderrightindex=()=>{
+          return this.state.cityindex.map((item,index)=>(
+                    <li className="city-index-item" key={item} onClick={()=>{
+                        this.cityListComponent.current.scrollToRow(index)
+                    }}>
+                        <span className={this.state.activeIndex === index ? 'index-active' : ''}>
+          {item === 'hot' ? '热' : item.toUpperCase()}
+                        </span>
+                    </li>
+          ))
+      }
+      onRowsRendered=({startIndex})=>{
+         if(startIndex!==this.state.activeIndex){
+             this.setState({
+                 activeIndex:startIndex
+             })
+         }
+      }
+    async componentDidMount(){
+        await this.getcity()
+        this.cityListComponent.current.measureAllRows()
     }
     async getcity(){
        const res=await axios.get('http://localhost:8088/area/city?level=1')
@@ -86,15 +128,22 @@ export default class Citylist extends React.Component{
      <AutoSizer>
          {({width,height})=>(
                 <List
+                ref={this.cityListComponent}
                 width={width}
                 height={height}
                 rowCount={this.state.cityindex.length}
-                rowHeight={80}
+                rowHeight={this.getrowheight}
                 rowRenderer={this.rowRenderer}
+                onRowsRendered={this.onRowsRendered}
+                scrollToAlignment="start"
                 />
          )}
      </AutoSizer>
-   
+     <ul className="city-index">
+         {this.renderrightindex()}
+     
+     </ul>
+     
         </div>
     }
 }
